a5e6aa0b04d8a5dae65bf241e9eb598488b930a7
Oliver Bielik <oliverbielik@MacBook-Pro-uzivatela-Oliver.local>
2022-06-20 15:36:38 +0200

    Sfunkčnenie nového zdroju dát (externá API)

--- App/apiReq.py
+++ App/apiReq.py
@@ -4,12 +4,15 @@ import requests
 BASE ="http://127.0.0.1:5000/"
 
 #Zdroj dát
-data = [{"userId": 1,"id": 2, "title": "Pozdrav", "body": 'Ahoj Ahoj Ahoj'},
+response = requests.get('https://jsonplaceholder.typicode.com/posts')
+data = response.json()
+
+'''data = [{"userId": 1,"id": 2, "title": "Pozdrav", "body": 'Ahoj Ahoj Ahoj'},
         {"userId": 2,"id": 4, "title": "Pijem", "body": "Mineralka je dobrá"}, 
         {"userId": 2,"id": 6, "title": "Oslavujeme", "body": "50-te narodeniny"},
         {"userId": 2,"id": 8, "title": "Skúšam auto", "body": "Mam nove auto"}, 
         {"userId": 5,"id": 10, "title": "Deravá guma", "body": "Opravujem bicykel"}, 
-        {"userId": 6,"id": 12, "title": "Pomoc", "body": "Ako opraviť auto"}]
+        {"userId": 6,"id": 12, "title": "Pomoc", "body": "Ako opraviť auto"}]'''
 
 #Požiadavky na server (BASE + " ") - definuje URL
 for i in range(len(data)):
